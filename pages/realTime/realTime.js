e5b56913847801f8fe47b65025b4ca9a1459e213
yisibugou <nihaozhouyi@163.com>
2021-09-02 17:24:16 +0800

    test

--- pages/realTime/realTime.js
+++ pages/realTime/realTime.js
@@ -5,14 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    var goods = [
+      {
+        img: "https://img.pddpic.com/gaudit-image/2021-08-06/9accb95f4955eedc1c5a34442299d57a.jpeg",
+        name: "香莎护发素干枯毛躁修复香水味持久学生男女顺滑大瓶容量高档香氛",
+        returnMoney: 2.9,
+        priceAfterCoupon: 22.9,
+        saleNum: "2万件",
+        tagList: [
+          "退货包运费",
+          "七天无理由"
+        ]
+      },
+      {
+        img: "https://img.pddpic.com/gaudit-image/2021-08-06/9accb95f4955eedc1c5a34442299d57a.jpeg",
+        name: "香莎护发素干枯毛躁修复香水味持久学生男女顺滑大瓶容量高档香氛",
+        returnMoney: 2.9,
+        priceAfterCoupon: 22.9,
+        saleNum: "2万件",
+        tagList: [
+          "质保三年",
+          "七天无理由"
+        ]
+      }
+    ];
+    var goodsListTemp = new Array();
+    for(var i = 0; i < 10; ++i) {
+      goodsListTemp.push(goods[i % 2]);
+    }
+    that.setData({
+      goodsList: goodsListTemp
+    })
   },
 
   /**
